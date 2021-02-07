e1931011f92230a87a91695e259a1557f79bdda3
Hanmei Tang <hmtang003@gmail.com>
2021-02-07 15:24:59 -0800

    web notes

---
+++ web/js/main.js
@@ -0,0 +1,31 @@
+alert('hello');
+
+// es 5
+// dynamic typing
+// primitive type:
+//      number (int + float)
+//      boolean
+//      string
+// reference type:
+//      Object
+//      array
+//      function
+var a = "123";
+
+var person = {
+    age: 40,
+    name: 'test'
+};
+
+console.log(person.age);
+person.age = '50';
+console.log(person.age);
+
+var ary = ["string", 12];
+ary.push('12');
+
+
+var fu = function() {};
+fu();
+
+
---
+++ web/js/type_in_js.js
@@ -0,0 +1,23 @@
+// es 5
+// dynamic typing
+// primitive type:
+//      number (int + float)
+//      boolean
+//      string
+// reference type:
+//      Object
+//      array
+//      function
+
+null;
+undefined;
+
+// Number
+var a = 1;
+var b = 3.24;
+
+// String
+var firstName = "Joe";
+var lastName = 'Curry';
+
+//
